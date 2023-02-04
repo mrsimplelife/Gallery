f37bbc4e7d352dd001c4d5ca04a65e64fa48284b
youme <parkddongbird@gmail.com>
2023-02-05 04:21:04 +0900

    fix : UIWindow(windowScene: windowScene)

--- Gallery/SceneDelegate.swift
+++ Gallery/SceneDelegate.swift
@@ -16,8 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        self.window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        self.window?.windowScene = windowScene
+        self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = GalleryController()
         self.window?.makeKeyAndVisible()
     }
